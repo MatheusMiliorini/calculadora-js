9cf8fd8c628c54b51535626fc65d84e12bb336ed
MatheusMiliorini <miliorinimatheus@gmail.com>
2018-08-08 12:52:40 -0300

    Lógica funcionando!

--- main.js
+++ main.js
@@ -12,9 +12,8 @@ resBoxDiv.appendChild(resBox);
 //Botões de 0 a 9
 var botoes = [];
 
-//Valores "cache" da calculadora
-var oldNum = null;
-var newNum = null;
+//Expressão algébrica que será passada para a função "eval"
+var exp = "";
 
 //Tipo de calculo que sera feito
 var acao = "";
@@ -39,59 +38,38 @@ function criaBotao(txt) {
 
 var btnMais = criaBotao('+');
 btnMais.addEventListener('click',function() {
-    oldNum = resBox.value;
-    acao = 'somar';
-    resBox.value = '';
+    exp += resBox.value + "+";
+    resBox.value = "";
 });
 
 var btnMenos = criaBotao('-');
 btnMenos.addEventListener('click',function() {
-    oldNum = resBox.value;
-    acao = 'subtrair';
-    resBox.value = '';
+    exp += resBox.value + "-";
+    resBox.value = "";
 })
 
 var btnVezes = criaBotao('x');
 btnVezes.addEventListener('click',function() {
-    oldNum = resBox.value;
-    acao = 'multiplicar';
-    resBox.value = '';
+    exp += resBox.value + "*";
+    resBox.value = "";
 });
 
 var btnDiv = criaBotao('/');
 btnDiv.addEventListener('click',function() {
-    oldNum = resBox.value;
-    acao = 'dividir';
-    resBox.value = '';
+    exp += resBox.value + "/";
+    resBox.value = "";
+
 });
 
 var btnIgual = criaBotao('=');
 btnIgual.addEventListener('click',function() {
-    newNum = parseFloat(resBox.value);
-    switch(acao) {
-        case 'somar':
-            resBox.value = parseFloat(oldNum) + parseFloat(newNum);
-            oldNum = resBox.value;
-            break;
-        case 'subtrair':
-            resBox.value = parseFloat(oldNum) - parseFloat(newNum);
-            oldNum = resBox.value;
-            break;
-        case 'multiplicar':
-            resBox.value = parseFloat(oldNum) * parseFloat(newNum);
-            oldNum = resBox.value;
-            break;
-        case 'dividir':
-            resBox.value = parseFloat(oldNum) / parseFloat(newNum);
-            oldNum = resBox.value;
-            break;
-    }
+    exp += resBox.value
+    resBox.value = eval(exp);
+    console.log(exp);
 });
 
 var btnLimpa = criaBotao('AC');
 btnLimpa.addEventListener('click',function() {
-    oldNum = null;
-    newNum = null;
-    acao = '';
-    resBox.value = null;
+    exp = "";
+    resBox.value = "";
 })
